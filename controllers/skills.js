23c5a1ad5b7e21bafdc9f4de64d3fc1edb33b3f3
Avesh Rathod <avesh@Aveshs-MacBook-Air.local>
2021-04-24 23:43:33 -0400

    lab completed

--- controllers/skills.js
+++ controllers/skills.js
@@ -17,7 +17,25 @@ function show(req, res) {
   });
 }
 
+function newSkill(req, res) {
+  res.render("skills/new");
+}
+
+function create(req, res) {
+  console.log(req.body);
+  Skill.create(req.body);
+  res.redirect("/skills");
+}
+
+function deleteSkill(req, res) {
+  Skill.deleteOne(req.params.id);
+  res.redirect("/skills");
+}
+
 module.exports = {
   index,
   show,
+  newSkill,
+  create,
+  delete: deleteSkill,
 };
--- models/skill.js
+++ models/skill.js
@@ -13,7 +13,19 @@ function getOne(id) {
   return skills.find((skill) => skill.id === parseInt(id));
 }
 
+function create(skill) {
+  skill.id = Date.now() % 1000000;
+  skills.push(skill);
+}
+
+function deleteOne(id) {
+  const idx = skills.findIndex((skill) => skill.id === parseInt(id));
+  skills.splice(idx, 1);
+}
+
 module.exports = {
   getAll,
   getOne,
+  create,
+  deleteOne,
 };
